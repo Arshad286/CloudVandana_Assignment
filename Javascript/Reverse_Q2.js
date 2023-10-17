6ff63248f5b69ad2d920816181afc923b6df8763
Arshad286 <87989820+Arshad286@users.noreply.github.com>
2023-10-17 20:39:08 +0530

    Reverse_Q2.js

--- Javascript/Reverse_Q2.js
+++ Javascript/Reverse_Q2.js
@@ -1,9 +1,28 @@
-const arr = [1, 2, 3, 4, 5, 6];
-
-function SortDescending(a, b) {
-    return b - a;
-}
-
-arr.sort(SortDescending);
-
-console.log("Array sorted in descending order:", arr);
+function sortDescending(arr)
+ {
+    arr.sort((a, b) => b - a);
+    return arr;
+  }
+  
+  const input = prompt("Enter numbers:");
+  
+  if (input)
+   {
+    const inputArray = input.split(',').map(Number);
+  
+    if (!inputArray.some(isNaN))
+     {
+      console.log("Input Array:", inputArray);
+  
+      const sortedArray = sortDescending(inputArray);
+      console.log("Sorted Descending Array:", sortedArray);
+    } 
+    else
+     {
+      console.log("Invalid input. Please enter valid numbers separated by commas.");
+    }
+  } 
+  else 
+  {
+    console.log("No input provided.");
+  }
